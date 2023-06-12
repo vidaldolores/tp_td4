e8cfa952b570445a93f0f6444177e2f1a7f0cbb8
chantulevi <chantulevi1@gmail.com>
2023-06-12 14:19:30 -0300

    "ej2 version ultima "

--- ej21.py
+++ ej21.py
@@ -1,33 +1,42 @@
 import argparse
 import socket
+from scapy.all import Raw
+from scapy.layers.http import *
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='HTTP Redirector')
-    parser.add_argument('-r', '--redirect', nargs='+', action='append', help='Define a redirect rule in the format "host:location"')
-    parser.add_argument('-p', '--port', type=int, default=8080, help='Port to run the server on (default: 8080)')
+    parser.add_argument('-r', '--redirect', action='append', default=[], help='Define a redirect rule in the format "host:location"')
+    parser.add_argument('-d', '--lista_archivo', action='append', default=[], help='')
+    parser.add_argument('-c', '--archivo', help='')
+   
     return parser.parse_args()
 
 def create_redirects(rules):
     redirects = {}
+    print(rules)
     if rules:
         for rule in rules:
-            if len(rule) == 2:
-                host, location = rule
-                redirects[host] = location
+            desde, hastaHTTPS, hasta = rule.split(":")
+            host = desde
+            location = hastaHTTPS + ':' + hasta
+            redirects[host] = location
     return redirects
 
-def handle_request(client_socket, redirects):
+def handle_request(request_data, redirects, client_socket, recive_archivo):
     # Recibo lo que me envió el cliente y lo decodifico
-    request_data = client_socket.recv(4096).decode('utf-8')
+   
+    scapy_pkt = HTTP(request_data) #IGUAL
+    raw = Raw(scapy_pkt) #IGUAL
+    payload = (raw[Raw].load).decode() #IGUAL
+   
+    if 'GET' in payload:
+        splits = payload.split(' ')
+        for x in splits:
+            if x.startswith('www.'):
+                host = x
+        host = host.strip()
 
-    # Extraigo el host de la solicitud
-    host = None
-    for line in request_data.split('\r\n'):
-        if line.startswith('Host: '):
-            host = line[6:]
-            break
-
-    if host in redirects:
+    if host in redirects.keys():
         new_location = redirects[host]
         print(f'[*] Request GET recibido (Host: {host})')
         print(f'[*] Respondiendo redirección hacia {new_location}')
@@ -36,36 +45,44 @@ def handle_request(client_socket, redirects):
             'Location: {}\r\n'
             'Connection: close\r\n\r\n'.format(new_location)
         )
-        client_socket.sendall(response.encode('utf-8'))
+        client_socket.sendall(bytes_encode(response))
+    elif host in recive_archivo:
+        with open(archivo, 'r') as file:
+            html_content:str = file.read()
+        response = (
+            'HTTP/1.1 200\r\n'
+            "Content-Type: text/html\r\n\r\n"
+        )
+        response += html_content
+        client_socket.sendall(bytes_encode(response))
     else:
         response = (
-            'HTTP/1.1 404 Not Found\r\n'
-            'Connection: close\r\n\r\n'
+            'HTTP/1.1 301 Moved Permanently\r\n'
+            'Location: {}\r\n'
+            'Connection: close\r\n\r\n https://'.format(host)
+           
         )
-        client_socket.sendall(response.encode('utf-8'))
+        client_socket.sendall(bytes_encode(response))
 
-    client_socket.close()
 
-def run_server(port, redirects):
-    # Creo un socket del servidor
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # Lo enlazo a una dirección y un puerto
-    server_socket.bind(('localhost', port))
-    # Escucho en el socket del servidor para conexiones entrantes
-    server_socket.listen(1)
-    print(f'Servidor en ejecución en el puerto {port}...')
+   
 
-    while True:
-        # Acepto la conexión entrante
-        client_socket, client_address = server_socket.accept()
-        handle_request(client_socket, redirects)
 
-    server_socket.close()
+args = parse_arguments()
+redirects = create_redirects(args.redirect)
+recive_archivo = set(args.lista_archivo)
+archivo = args.archivo
+port = 80
+# Creo un socket del servidor
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Lo enlazo a una dirección y un puerto
+server_socket.bind(('0.0.0.0', port))
+# Escucho en el socket del servidor para conexiones entrantes
+server_socket.listen()
+print(f'Servidor en ejecución en el puerto {port}...')
+client_socket, client_address = server_socket.accept()
 
-def main():
-    args = parse_arguments()
-    redirects = create_redirects(args.redirect)
-    run_server(args.port, redirects)
-
-if __name__ == '__main__':
-    main()
+while True:
+    # Acepto la conexión entrante
+    request_data = client_socket.recv(1024)
+    handle_request(request_data, redirects, client_socket, recive_archivo)
